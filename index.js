01f94df79bbe68f839b13173288bacad4d1272ca
tadzyla <tadas.zvinklys@gmail.com>
2021-04-12 23:42:35 +0100

    generating grid

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const { World, Engine, Render, Runner, Bodies, MouseConstraint, Mouse } = Matter;
+const { World, Engine, Render, Runner, Bodies } = Matter;
 
-
+const cells = 3;
 const width = 800;
 const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        wireframes: false,
+        wireframes: true, 
         width,
         height
     }
@@ -18,32 +19,43 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
-
 //Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
-    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
+    Bodies.rectangle(width/2, 0, width, 40, {isStatic: true}),   //top wall
+    Bodies.rectangle(width/2, height, width, 40, {isStatic: true}),  //bottom wall
+    Bodies.rectangle(0, height / 2, 40, height, {isStatic: true}),         // left wall
+    Bodies.rectangle(width, height / 2, 40, height, {isStatic: true})       // right wall      
 ];
 
 World.add(world, walls);
 
-//Shapes
-for(let i=0; i<30; i++) {
-    if(Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(
-            Math.random() * width, 
-            Math.random() * height,
-            50, 50))
-    } else {
-        World.add(world, Bodies.circle(
-            Math.random() * width, 
-            Math.random() * height,
-            30))
-    }
-    
-}
+
+// MAZE GENERATION
+
+// Making grid
+// I way
+// const grid = [];
+
+// for(let i = 0; i < 3; i++){      
+//     grid.push([]);                  // pushed indivudual row
+//     for(let j = 0; j < 3; j++){
+//         grid[i].push(false);
+//     }
+// }
+
+// II way
+ const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false)); 
+
+// making vertical walls
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+// making horizontal walls
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells  ).fill(false));
+
+    console.log(verticals, horizontals);
