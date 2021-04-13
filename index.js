bbb9768ecfa7081d50056210191ec43c16bee6ff
tadzyla <tadas.zvinklys@gmail.com>
2021-04-14 00:02:10 +0100

    making walls

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { World, Engine, Render, Runner, Bodies } = Matter;
 
 const cells = 3;
-const width = 800;
+const width = 600;
 const height = 600;
+const unitLength = width / cells;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -19,9 +20,9 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-//Walls
+// Maze boundaries
 const walls = [
-    Bodies.rectangle(width/2, 0, width, 40, {isStatic: true}),   //top wall
+    Bodies.rectangle(width/2, 0, width, 40, {isStatic: true}),       //top wall
     Bodies.rectangle(width/2, height, width, 40, {isStatic: true}),  //bottom wall
     Bodies.rectangle(0, height / 2, 40, height, {isStatic: true}),         // left wall
     Bodies.rectangle(width, height / 2, 40, height, {isStatic: true})       // right wall      
@@ -109,7 +110,7 @@ const horizontals = Array(cells - 1)
             }
 
             // If we have visited that neighbor, continue with other neighbor
-            if(grid[nextRow][nextColumn]){  // if this is true
+            if(grid[nextRow][nextColumn]) {  // if this is true
                 continue;
             }
             // Remove wall from horizontals or verticals
@@ -127,7 +128,29 @@ const horizontals = Array(cells - 1)
         }
     };
     
+  stepThroughCell(startRow, startColumn);
 
-    stepThroughCell(startRow, startColumn);
+
+
+// Drawing wall in a cell
+
+  horizontals.forEach((row, rowIndex) => {    
+      row.forEach((open, columnIndex) => { 
+          if(open){
+              return;
+          }
+          
+          const wall = Bodies.rectangle(
+              columnIndex * unitLength + unitLength / 2,  // X coordinate
+              rowIndex * unitLength + unitLength,         // Y coordinate
+              unitLength,                                 // length of the wall
+              10,
+              {
+                  isStatic: true     //doesnt fly around
+              }
+              );
+            World.add(world, wall);
+        });
+  });
     
     
