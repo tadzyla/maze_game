d0dfd1246ddf132446751bbaeb4652f603521832
tadzyla <tadas.zvinklys@gmail.com>
2021-04-13 23:18:04 +0100

    forming maze

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ World.add(world, walls);
 
 // MAZE GENERATION
 
+const shuffle = (arr) => {
+    let counter = arr.length;
+    while(counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+    return arr;
+}
+
 // Making grid
 // I way
 // const grid = [];
@@ -56,6 +70,64 @@ const verticals = Array(cells)
 // making horizontal walls
 const horizontals = Array(cells - 1)
     .fill(null)
-    .map(() => Array(cells  ).fill(false));
+    .map(() => Array(cells).fill(false));
 
-    console.log(verticals, horizontals);
+    const startRow = Math.floor(Math.random() * cells);      // Starting point
+    const startColumn = Math.floor(Math.random() * cells);   // Starting point
+
+
+
+    const stepThroughCell = (row, column) => {
+
+        // if I have visited cell at [row, column] then return
+        if(grid[row][column]){
+            return;
+        }
+
+        // Mark this cell as being visited
+        grid[row][column] = true;
+
+        // Assemble randomly ordered list of neighbors
+        const neighbours = shuffle([
+            [row - 1, column, 'up'],      
+            [row, column + 1, 'right'],      
+            [row + 1, column, 'down'],      
+            [row, column - 1, 'left']       
+        ]);
+        
+        // For each neighbor...
+        for(let neighbor of neighbours) {
+            const[nextRow, nextColumn, direction] = neighbor;  // next neighbor with thinking off
+
+            // Check if neighbor is out of bounds
+            if( row < 0      || 
+                row >= cells || 
+                column < 0   || 
+                column >= cells
+                ){
+                continue;  // continue with other neighbor
+            }
+
+            // If we have visited that neighbor, continue with other neighbor
+            if(grid[nextRow][nextColumn]){  // if this is true
+                continue;
+            }
+            // Remove wall from horizontals or verticals
+            if(direction === 'left'){
+                verticals[row][column - 1] = true;
+            } else if (direction === 'right'){
+                verticals[row][column] = true;
+            } else if(direction === 'up'){
+                horizontals[row - 1][column] = true;
+            } else if(direction === 'down'){
+                horizontals[row][column] = true;
+            }
+
+            stepThroughCell(nextRow, nextColumn);
+        }
+    };
+    
+
+    stepThroughCell(startRow, startColumn);
+    
+    
